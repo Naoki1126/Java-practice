20ae84abad95da0601b48ccd4e9a746e840567b3
NaokiMaruyama <m.naoki85@gmail.com>
2020-07-20 15:35:49 +0900

    [Finish]curriculum/JavaCariculum/src/Chapter04.java

---
+++ curriculum/JavaCarriculum/src/Chapter04.java
@@ -0,0 +1,9 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "丸山直城";
+		System.out.println(name);
+		name = "こんにちは";
+		System.out.println(name);
+	}
+}
---
+++ curriculum/JavaCarriculum/src/Variable01.java
@@ -0,0 +1,17 @@
+
+public class Variable01 {
+	public static void main(String[] args) {
+        int num1; //int型の変数num1を用意
+        String str1; //String型の変数 str1を用意
+        num1 = 10; //変数num1に10を代入
+        str1 = "Hello World!"; //変数str1にHello Worldを代入
+        System.out.println(num1); //num1の出力
+        System.out.println(str1); //str1の出力
+
+        num1 = 20; //num1に20を再代入;
+        System.out.println(num1); //変数num1の出力
+
+        int num2 = 30; //int型の変数num2を定義、30を代入
+        System.out.println(num2); //変数num2の出力
+    }
+}
