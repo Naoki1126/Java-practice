0eaa2abfcec31d10ba55c8eaa14c2701aa7dd98a
NaokiMaruyama <m.naoki85@gmail.com>
2020-07-20 15:20:22 +0900

    [Finish]curriculum/java_基礎_3章

--- curriculum/JavaCarriculum/src/Chapter02.java
+++ curriculum/JavaCarriculum/src/Chapter02.java
@@ -1,7 +1,14 @@
 
 public class Chapter02 {
+	//メソッドブロックの始まり↓（インデントでわかりやすく)
 	public static void main(String[] args) {
+		//メソッドの処理を追加↓(インデントでわかりやすく)
 		System.out.println("Hello Webcamp");
 		System.out.println("Hello Java!");
+		System.out.println("Webcamp");
+
 	}
+//  ↑メソッドブロックの終了
 }
+//↑クラスの終了
+
