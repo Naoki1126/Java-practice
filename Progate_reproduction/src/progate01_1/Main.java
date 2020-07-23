2eb793adcd6d0163243eb6031907c212ee540419
NaokiMaruyama <m.naoki85@gmail.com>
2020-07-23 16:00:37 +0900

    [復習]Progate/java_study_1-3

---
+++ Progate_reproduction/src/progate01_1/Main.java
@@ -0,0 +1,10 @@
+package progate01_1;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		//Hello javaを出力
+		System.out.println("Hello Java");
+	}
+
+}
---
+++ Progate_reproduction/src/progate01_2/Main.java
@@ -0,0 +1,10 @@
+package progate01_2;
+
+public class Main {
+	public static void main(String[] args) {
+		//この文章をコメントアウトしてください
+		
+		//こんにちはJavaと出力
+		System.out.println("こんにちは、Java");
+	}
+}
