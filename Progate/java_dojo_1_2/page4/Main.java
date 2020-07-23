6d538a11329b464b1a1485e5146c37f1764446dd
NaokiMaruyama <m.naoki85@gmail.com>
2020-07-23 19:47:17 +0900

    [復習]Progate/java_dojo_1_2

---
+++ Progate/java_dojo_1_2/page4/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("名前：");
+    String firstName = scanner.next();
+    String lastName = scanner.next();
+    System.out.println("名前は" + fullName(firstName,lastName) +"です");
+  }
+  
+  public static String fullName(String firstName,String lastName){
+    return firstName + " " + lastName;
+  }
+}
---
+++ Progate/java_dojo_1_2/page5/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("名前：");
+    String firstName = scanner.next();
+    System.out.print("名字：");
+    String lastName = scanner.next();
+    System.out.println("年齢：");
+    int age = scanner.nextInt();
+    System.out.println("名前は" + fullName(firstName,lastName) +"です");
+    System.out.println("年齢は" + age + "歳です");
+    if (age >= 20){
+      System.out.println("成年者です");
+    }else{
+      System.out.println("未成年者です");
+    }
+  }
+  
+  public static String fullName(String firstName,String lastName){
+    return firstName + " " + lastName;
+  }
+}
